8aad789b4f331fa19618d674738318b47cea94db
jcluo <voluntarin@gmail.com>
2015-03-08 21:08:57 +0800

    更新內容，尚有問題

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,70 +2,69 @@
 
 $.fn.extend({
     dropdown: function (options) {
-
+        
+        options = options || {};
+        options.ajaxType = (options.ajaxType && options.ajaxType.toUpperCase() == 'POST') ? 'post' : 'get';
         return this.each(function () {
 
 
             var $this = $(this).hide();
-            var $wrap = $('<div class="btn-group dropdown">').insertAfter($this);
+            var $wrap = $('<div class="btn-group dropdown dropdown-select">')
+                .css('width', options.width || 'auto')
+                .insertAfter($this);
             var $button = $('<button type="button">')
-                            .attr({
-                                'class': 'btn btn-default dropdown-toggle',
-                                'aria-expanded': 'false'
-                            })
-                            .css('width', options.width || 'auto')
-                            .html(($this.attr('title') || '請選擇'))
-                            .appendTo($wrap);
+                .attr({
+                    'class': 'btn btn-default dropdown-toggle',
+                    'aria-expanded': 'false'
+                })
+                .html(($this.attr('title') || '請選擇'))
+                .appendTo($wrap);
             var $menu = $('<ul class="dropdown-menu" role="menu"/>')
                             .appendTo($wrap);
-            var $search = $('<input type="text" class="form-control">');
+            var $field = $('<input type="text" class="form-control">');
             var $line = $('<li class="divider">');
-            var $clear = $('<button class="btn btn-default">取消選取</button>');
-            var $finish = $('<button class="btn btn-info">完成</button>');
+            var $clear = $('<button type="button" class="btn btn-default">取消選取</button>');
+            var $search = $('<button type="button" class="btn btn-info">查詢</button>');
             var $items = $('<li class="dropdown-items"/>');
             var $loading = $('<li class="dropdown-loading" />');
 
-            $this.data('dropdown', $wrap);
 
-            $menu.append($('<li>').append($search))
+            $menu.append($('<li>').append($field))
                 .append('<li class="divider">')
-                .append($('<li class="options">').append([$clear, $finish]))
+                .append($('<li class="options">').append([$clear, $search]))
                 .append($line)
                 .append($items)
                 .append($loading);
 
-            if (! options.source) {
-                options.source = [];
-                $this.find('option').each(function() {
-                    options.source.push({value: this.value, name: this.innerHTML});
-                });
-            }
+
+            $this.data('dropdown', $wrap);
+            $this.data('items', []);
+
 
             $button.bind('click', function () {
                 if ($menu.is(':visible')) {
-                    $this.trigger('dropdown-hide');
+                    $this.trigger('dds.hide');
                 } else {
-                    $this.trigger('dropdown-show');
-
+                    $this.trigger('dds.show');
                 }
             });
             
             $clear.bind('click', function () {
-                $this.trigger('set-value', [null]);
+                $this.trigger('dds.setValue', [null]);
             });
 
-            $finish.bind('click', function () {
-                $this.trigger('dropdown-hide');
+            $search.bind('click', function () {
+                $this.trigger('dds.getSource');
             });
 
             $(document).bind('mousedown', function (e) {
                 if ($(e.target).parents('.dropdown').length === 0) {
-                    $this.trigger('dropdown-hide');
+                    $this.trigger('dds.hide');
                 }
             });
 
 
-            $search.bind('input', function () {
+            $field.bind('input', function () {
 
                 $(this)
                     .attr('title', ' 按 Enter 查詢 ')
@@ -77,21 +76,21 @@ $.fn.extend({
                     $(this)
                         .removeAttr('title')
                         .tooltip('destroy');
-
-                    $this.trigger('get-source');
+                    $this.trigger('dds.getSource');
+                    e.preventDefault();
 
                 }
 
             })
 
             $this
-                .bind('dropdown-hide', function () {
+                .bind('dds.hide', function () {
                     $menu.hide();
                 })
-                .bind('dropdown-show', function () {
+                .bind('dds.show', function () {
                     $menu.show();
                 })
-                .bind('set-value', function (e, value) {
+                .bind('dds.setValue', function (e, value) {
                     var name = [];
                     var items = $this.data('items');
                     $items.find('.selected').removeClass('selected');
@@ -109,36 +108,37 @@ $.fn.extend({
                     }
 
                     $this.val(value);
-                    $button.html(name);
+                    $button.html(name || '請選擇');
 
                 })
-                .bind('get-source', function () {
+                .bind('dds.clear.noSelect', function () {
 
-                    var query = $(this).val();
+                })
+                .bind('dds.getSource', function () {
 
+                    var query = $field.val();
                     if (typeof options.source === 'string') {
 
-                        $.ajax(options.source, {data: {query: query}, dataType: 'json'})
+                        $.ajax(options.source, {type: options.ajaxType , data: {query: query}, dataType: 'json'})
                             .done(function (request) {
-                                $this.trigger('load-data', [request]);
+                                $this.trigger('dds.loadData', [request]);
                             })
                             .fail(function (request) {
                                 console.log(request);
                             });
                     } else {
-                        $this.trigger('load-data', [options.source]);
+
+                        $this.trigger('dds.loadData', [options.source]);
+
                     }
 
                 })
-                .bind('load-data', function (e, data) {
+                .bind('dds.loadData', function (e, data) {
 
-                    var query = $search.val();
-
-                    $items.find('.item').not('.selected').remove();
-                    $menu.find('option').not(':selected').remove();
-
-                    $this.data('items', []);
+                    var query = $field.val();
                     var items = $this.data('items');
+
+                    $this.trigger('dds.clear.noSelect');
 
                     $.each(data, function (i, o) {
 
@@ -147,11 +147,10 @@ $.fn.extend({
                         }
 
 
-                        if ($items.find('[item-value=' + o.value + ']').length === 0) {
+                        if (! items[o.value]) {
 
                             $('<option/>')
                                 .val(o.value)
-                                .html(o.name)
                                 .appendTo($this);
 
 
@@ -164,7 +163,6 @@ $.fn.extend({
 
                                     var selected = $this.val() || [];
                                     var value = $(this).data('item-data').value.toString();
-
                                     if ($this.prop('multiple')) {
                                         var idx = selected.indexOf(value);
 
@@ -176,20 +174,32 @@ $.fn.extend({
 
                                     } else {
                                         selected = value;
-                                        $this.trigger('dropdown-hide');
+                                        $this.trigger('dds.hide');
                                     }
-                                    $this.trigger('set-value', [selected]);
+                                    $this.trigger('dds.setValue', [selected]);
 
                                 });
                         }
                     });
-
-                    $this.trigger('set-value', $this.val());                    
+                    $this.trigger('dds.setValue', [$this.val()]);                    
 
             });
 
             
-            $this.trigger('get-source');
+            var items = [];
+            var def_value = $this.val();
+            $this.find('option').each(function() {
+                items.push({ value: this.value, name: this.innerHTML});
+            });
+            $this.find('option').remove();
+            options.source = options.source || items;
+
+            if (items.length) {
+                $this.trigger('dds.loadData', [items]);
+                $this.trigger('dds.setValue', [def_value]);
+            } else {
+                $this.trigger('dds.getSource');
+            }
 
 
         });
